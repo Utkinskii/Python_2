5e835b6d65f06e673eb2eb72b678230f977a3bf4
Utkinskii <sakunov1707@gmail.com>
2023-10-04 16:42:32 +0300

    Добавьте файлы проекта.

---
+++ Python_2/Python_2.py
@@ -0,0 +1,10 @@
+a1 = "Dryopithecus"
+a2 = "Ramapithecus"
+a3 = "Australopithecus"
+a4 = "Homo habilis"
+a5 = "Homo erectus"
+a6 = "Neanderthal man"
+a7 = "Neoanthropus"
+stage = print (input("Enter the stages of human development in order: "))
+print (a1, a2, a3, a4,a5, a6, a7, sep="=>")
+#  сожелени€ € до конца не пон€л как прапвильно строить такие тесты с проверкой, знаний функций наверное не хватает, € попыталс€ через if но это очень длинный код получаетс€ на проверку всех переменных.
